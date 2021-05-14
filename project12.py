08a46633e6b99f53ebe1340166c95caadb2da175
Juune02 <mykj6573@gmail.com>
2021-05-15 00:50:49 +0900

    project12 is updated by Juune02!

--- project12.py
+++ project12.py
@@ -3,24 +3,33 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-print("구독자 수가 궁금한 채널명을 입력")
+print("유튜브 채널을 검색하기")
 string=input(">> : ")
 
 URL='https://www.youtube.com/'
 #options = webdriver.ChromeOptions()
 #options.add_argument('headless')
 driver = webdriver.Chrome(executable_path='C:/Users/mykj6/OneDrive/바탕 화면/202155130_namjuhyeong/chromedriver.exe')#,options=options)
-driver.get(url=URL)
 
+driver.get(url=URL)
 search_box = driver.find_element_by_name("search_query")
 search_box.send_keys(string)
-search_btn=driver.find_element_by_class_name("style-scope ytd-searchbox")
-search_btn.send_keys(Keys.ENTER)
+search_btn=driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
+search_btn.click()
 
+driver.get(url = driver.current_url)
+setting=driver.find_element_by_xpath('//*[@id="container"]/ytd-toggle-button-renderer')
+setting.click()
+set_ch=driver.find_elements_by_xpath('//*[@id="label"]')
+set_ch6=set_ch[6]
+set_ch6.click()
 
 driver.get(url = driver.current_url)
 channel = driver.find_element_by_xpath('//*[@id="img"]')
 channel.click()
+
 driver.get(url = driver.current_url)
+name=driver.find_element_by_xpath('//*[@id="channel-name"]')
+print("채널명:",name.text)
 subscriber = driver.find_element_by_xpath('//*[@id="subscriber-count"]')
-print(subscriber.text)
+print(subscriber.text,"입니다.")
